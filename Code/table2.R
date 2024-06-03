0f11cfd59c86af6b1a0a37882341fa4e4236c6a6
Mary Lofton <melofton@vt.edu>
2024-06-02 21:49:31 -0400

    creating data table to populate table 2 of cooperator report

---
+++ Code/table2.R
@@ -0,0 +1,26 @@
+# Table 2 data
+# Author: Mary Lofton
+# Date: 02JUN24
+
+# Purpose: pull data needed to populate Table 2. Reviewed studies
+
+# load packages
+library(tidyverse)
+library(lubridate)
+
+# read in data
+dat <- read_csv("./Data/Table2Data.csv") %>%
+  select("Covidence #","Study ID","Aim of study","How many waterbodies are in this study?",
+         "What biotic variables were measured?") %>%
+  rename(cov_num = `Covidence #`)
+dat2 <- read_csv("./Data/powell_litreview_edited2.csv") %>%
+  select(cov_num, mgmt_concerns, mgmt_strat)
+
+dat3 <- left_join(dat, dat2, by = c("cov_num")) %>%
+  distinct() %>%
+  rename(study_id = `Study ID`,
+         aim = `Aim of study`,
+         num_waterbodies = `How many waterbodies are in this study?`,
+         biotic_vars = `What biotic variables were measured?`) %>%
+  filter(!(study_id == "Carvalho 2022" & mgmt_concerns == "water level fluctuation"))
+write.csv(dat3, "./Data/Table2.csv", row.names = FALSE)
