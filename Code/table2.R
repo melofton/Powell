7f4c5e75bfd4402c9e2f933a84796203a1d59001
Mary Lofton <melofton@vt.edu>
2024-10-08 13:36:56 -0400

    table 2 revisions

--- Code/table2.R
+++ Code/table2.R
@@ -12,30 +12,32 @@ library(rlang)
 ## Table 2 ----
 # read in data
 dat <- read_csv("./Data/litdata_cleaned.csv") %>%
-  select(cov_num, res_name, country, lat_dd, long_dd, study_id) %>%
+  select(cov_num, res_name, country, lat_dd, long_dd, study_id, delta_water_level) %>%
   mutate(lat_dd = round(as.numeric(lat_dd),2),
          long_dd = round(as.numeric(long_dd),2)) %>%
   mutate(lat_dd = ifelse(is.na(lat_dd),"NR",lat_dd),
-         long_dd = ifelse(is.na(long_dd),"NR",long_dd))
+         long_dd = ifelse(is.na(long_dd),"NR",long_dd),
+         delta_water_level = ifelse(delta_water_level %in% c(NA, "not reported"),"NR",delta_water_level))
 
 dat2 <- read_csv("./Data/mosaic_plot_data_05SEP24.csv")
 
 dat3 <- left_join(dat2, dat, by = c("cov_num","res_name")) %>%
   select(res_name, country, lat_dd, long_dd, trophic_status_mosaic, increase_decrease_mosaic, 
-         increase_phyto, increase_cyano, study_id) %>%
+         delta_water_level, increase_phyto, increase_cyano, study_id) %>%
   rename(`Lake or Reservoir Name` = res_name,
          Country = country,
          `Latitude (decimal degrees)` = lat_dd,
          `Longitude (decimal degrees)` = long_dd,
          `Trophic Status` = trophic_status_mosaic,
          `Did water level increase or decrease?` = increase_decrease_mosaic,
+         `Water level change (m)` = delta_water_level,
          `Did phytoplankton increase?` = increase_phyto,
          `Did cyanobacteria increase?` = increase_cyano,
          `Study ID` = study_id) %>%
   arrange(`Lake or Reservoir Name`)
-write.csv(dat3, "./Data/Table2_05SEP24.csv", row.names = FALSE)
+write.csv(dat3, "./Data/Table2_08OCT24.csv", row.names = FALSE)
 
-tab <- read_csv("./Data/Table2_05SEP24.csv")
+tab <- read_csv("./Data/Table2_08OCT24.csv")
 
 
 ## Previous Table 2 (now in supplement)
